ce56d31f86b09b1bb92ae71fbede6109f830ad38
andre <andreivan.santos@gmail.com>
2019-09-28 21:57:18 -0300

    Test iterate through the json

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # API_Pokedex
 =============================================================
 This file is to guide you in order to setup and run the project automation
-- This framework use the tools Supertest, Mocha and Chai and is build using Javascript language
-
+- The tests are to validate the API: https://pokeapi.co/docs/v2.html/
+- This framework use the tools Supertest, Mocha and Chai and it was built using Javascript language
 
 =============================================================
 BUILD the environment:
 
 - First of all clone this GIT repository
-- Then access the path to the file package.json
+- Then access the path to the file package.jsons
 - Perform the command npm install
+
 =============================================================
 In order to run the automation:
 
 - Execute the command npm test
-
 
 =============================================================
--- Test/api/pokemon.js
+++ Test/api/pokemon.js
@@ -1,20 +1,29 @@
 var env = require('../helper/index');
+var moveName = require('../testData/pokemonData');
 var request = require('supertest')(env);
 var expect = require('chai').expect;
 
 describe("Look up Pokemons", function () {
 
-  it("should locate a correct pokemon by name", function (done) {  
-    let pokeName = "beedrill"    
+  it("should locate a correct pokemon by name", function (done) {
+    let pokeName = "beedrill"
     request
       .get('/pokemon/' + pokeName).end(function (err, res) {
+        let amountOfMoves = 0;
+        for (i = 0; i < res.body.moves.length; i++) {
+          let moveNameApi = res.body.moves[i].move.name
+          let moveNameData = moveName[i];
+          expect(moveNameApi).to.equal(moveNameData);
+          amountOfMoves++
+        }
+        expect(amountOfMoves).to.equal(75);
         expect(res.statusCode).to.equal(200);
         expect(res.body.name).to.equal(pokeName);
         done(err);
       })
   });
 
-  it("should locate a correct pokemon by id", function (done) {      
+  it("should locate a correct pokemon by id", function (done) {
     request
       .get('/pokemon/' + 15).end(function (err, res) {
         expect(res.statusCode).to.equal(200);
@@ -23,7 +32,7 @@ describe("Look up Pokemons", function () {
       })
   });
 
-  it("should not locate a pokemon by name", function (done) {    
+  it("should not locate a pokemon by name", function (done) {
     request
       .get('/pokemon/' + "beedrill1").end(function (err, res) {
         expect(res.statusCode).to.equal(404);
